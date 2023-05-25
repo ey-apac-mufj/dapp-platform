36e2ef08c163988305ea61a5db396a549ab1555f
Joe Meng <xiangying.meng@jp.ey.com>
2023-05-25 21:42:08 +0900

    hot fix host and css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CommunityDetails from "./CommunityDetails";
-import Home from "./Home";
+// import Home from "./Home";
 import Landing from "./Landing";
+import "./styles/Home.css";
 
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/old" element={<Home />} />
+          {/* <Route path="/old" element={<Home />} /> */}
           <Route path="/" element={<Landing />} />
           <Route path="/community-details" element={<CommunityDetails />} />
         </Routes>
