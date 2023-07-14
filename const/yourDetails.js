d646043e0541125fb43cb214cbf4878d9118f41f
Gourav Chatterjee <gourav.chatterjee1@gds.ey.com>
2023-07-14 13:18:59 +0530

    feat: Login api integration w/o actual API

--- const/yourDetails.js
+++ const/yourDetails.js
@@ -4,6 +4,8 @@ export const TWFactoryAddress = "0x3084B8B244D2d4a43fbF137165580FD4676E08c2";
 export const TWApiKey =
   "Pp-yMCi3csVA2NBc5trHpLHyT5uErD1q-ZDVzriH4gPEhkW-sGdPXT72UFXDStwm6RwHY2ZqwgqKip-dZoCM9Q";
 export const TWClientID = "a3d61ce5edfbb566f88f6a3e8076ef81";
+
+export const apiurl = "https://medidemoapi.com";
 export const activeChain = Goerli;
 
 export const editionDropAddress = "0x8D9919db3CD6aF84e8A12CedC3c5A694Bf026aB8";
